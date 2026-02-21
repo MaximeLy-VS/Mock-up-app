2646c110e8d4fb7845155914c18b8236abe3484e
MaximeLy-VS <lyon.maxime@gmail.com>
2026-02-21 10:30:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -201,8 +201,8 @@ export default function App() {
                 <ImageIcon className="text-white" size={24} />
               </div>
               <div>
-                <h1 className="text-2xl font-black tracking-tight text-slate-800">Mock-up – Mission</h1>
-                <p className="text-slate-400 text-[10px] font-bold uppercase tracking-[0.2em]">Convertisseur et générateur de vignette</p>
+                <h1 className="text-2xl font-black tracking-tight text-slate-800">Mock up&nbsp;–&nbsp;Mission</h1>
+                <p className="text-slate-400 text-[10px] font-bold uppercase tracking-[0.2em]">Convertisseur et générateur de&nbsp;vignette</p>
               </div>
             </div>
           </header>
